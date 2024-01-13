a74bc70b3b56b1f280115eddedb17644e44c7818
karen4ik <karenavetisyan.200129@gmail.com>
2024-01-13 19:17:30 +0300

    Added localization

---
+++ lib/core/l10n/all_locale.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AllLocale {
+  AllLocale();
+
+  static final all = [
+    const Locale('en', 'US'), // en is language Code and US is the country code
+    const Locale('ru', 'RU'),
+  ];
+}
